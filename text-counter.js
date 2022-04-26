8dc8f334680a4220afe160bd023aa7b5fc3c0975
Murugan Ayyanar <muruganayyanar07@gmail.com>
2022-04-26 19:45:42 -0400

    commit

---
+++ text-counter.js
@@ -0,0 +1,15 @@
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/{}/g, function () {
+      return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+};
+
+var nom = document.getElementById("num-of-words");
+
+function count(text_input){
+  
+  alert('This is what you inputted: {}'.format(text_input));
+  let all_chars = text_input.split(" ");
+  nom.innerHTML = "The Number Of Words In Your Text: {}".format(all_chars.length);
+};
