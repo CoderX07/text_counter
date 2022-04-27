b8400af762a045e34fad9c635774decc4f2803da
CoderX07 <muruganayyanar07@gmail.com>
2022-04-27 13:17:22 -0400

    Adding Sentence Counter

--- text-counter.js
+++ text-counter.js
@@ -6,9 +6,12 @@ String.prototype.format = function () {
 };
 
 var nom = document.getElementById("num-of-words");
+var nos = document.getElementById("num-of-sent");
 
 function count(text_input){
   alert('This is what you inputted: {}'.format(text_input));
   let all_chars = text_input.split(" ");
+  let all_sent = text_input.split(".");
   nom.innerHTML = "The Number Of Words In Your Text: {}".format(all_chars.length);
+  nos.innerHTML = "The Number Of Sentences In Your Text: {}".format(all_sent.length);
 };
