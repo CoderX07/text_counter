2cc2ab4370d903114de87563fbaa129c260fa177
CoderX07 <muruganayyanar07@gmail.com>
2022-04-27 13:37:15 -0400

    Update text-counter.js

--- text-counter.js
+++ text-counter.js
@@ -10,14 +10,20 @@ var nos = document.getElementById("num-of-sent");
 
 function count(text_input){
   alert('This is what you inputted: {}'.format(text_input));
-  let all_chars = text_input.split(" ");
+  let all_words = text_input.split(" ");
   let all_sent = text_input.split(".");
   let len_all_sent = all_sent.length;
+  let len_all_words = all_words.length;
   for (var i = 0; i < len_all_sent; i++){
     if (all_sent[i] == " ") {
       len_all_sent--;
     };
   };
-  nom.innerHTML = "The Number Of Words In Your Text: {}".format(all_chars.length);
+  for (var i = 0; i < len_all_words; i++){
+    if ((all_words[i] == ".") || (all_words[i] == '"')) {
+      len_all_words--;
+    };
+  };
+  nom.innerHTML = "The Number Of Words In Your Text: {}".format(len_all_words);
   nos.innerHTML = "The Number Of Sentences In Your Text: {}".format(len_all_sent);
 };
