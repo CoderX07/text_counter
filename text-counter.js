9201e4c1b7e01931378c9059c8d3a83b9abe54b2
CoderX07 <muruganayyanar07@gmail.com>
2022-04-30 09:46:08 -0400

    Update text-counter.js

--- text-counter.js
+++ text-counter.js
@@ -23,6 +23,7 @@ function count(text_input){
   let all_sent = text_input.split(".");
   let len_all_sent = all_sent.length;
   let len_all_words = all_words.length;
+  all_words = all_words.split("↵");
   for (var i = 0; i < len_all_sent; i++){
     if ((all_sent[i] == " ") || (all_sent[i] == "") || (all_sent[i] == '"') || (all_sent[i] == "”")){
       len_all_sent--;
